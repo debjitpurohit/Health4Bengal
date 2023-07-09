0e62094978b32839f72cdc12d2be5cb7de6a6bb4
DEBJIT PUROHIT <92798917+debjitpurohit@users.noreply.github.com>
2023-07-10 02:09:49 +0530

    added 2

--- public/index.js
+++ public/index.js
@@ -10,7 +10,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
